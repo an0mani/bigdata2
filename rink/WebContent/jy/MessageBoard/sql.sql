13c4bf1670cd012fc17a3e18a54dc922cae8338a
윤지호 <jhyoon1128@gmail.com>
2017-12-02 15:32:31 +0900

    commit 오류수정중.

--- rink/WebContent/jy/MessageBoard/sql.sql
+++ rink/WebContent/jy/MessageBoard/sql.sql
@@ -1,52 +1,6 @@
 select * from BABY_MESSAGEBOARD
 
- create table baby_messageboard(
- id varchar2(10),
- num number,
- title varchar2(30),
- text varchar2(1000),
- wdate varchar2(10),
- filename varchar2(30)
- );
  
- 
- 
-  create table baby_sellboard(
- id varchar2(10),
- num number,
- title varchar2(30),
- text varchar2(1000),
- wdate varchar2(10),
- filename varchar2(30)
- );
- 
- create table baby_diary(
-    id varchar2(10),
-    num number,
-    title varchar2(30),
-    text varchar2(1000),
-    wdate varchar(10),
-    filename varchar2(30)
-);
-
-create table baby_notice(
-    num number,
-    title varchar2(30),
-    text varchar2(1000),
-    wdate varchar(10)
-);
-
-create table baby_member(
-   m_id varchar(30),
-   m_pw varchar(30),
-   m_name varchar(30),
-   m_sex varchar(10),
-   m_phone varchar(30),
-   m_blood varchar(10),
-   m_weight number,
-   m_height number,
-   m_birth varchar(30)
-);
 
 drop table baby_messageboard;
 drop table baby_sellboard;
@@ -67,7 +21,8 @@ create table baby_sellboard(
     title varchar2(50),
     text varchar2(1000),
     wdate varchar2(10),
-    filename varchar2(50)
+    filename varchar2(50),
+    kind varchar2(30)
  );
  
 create table baby_diary(
